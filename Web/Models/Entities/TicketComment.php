9a99aeb3fc2484e00158b63197406c45740decfe
Alma Armas <celestine@vriska.ru>
2020-07-16 20:09:48 +0000

    Make support agents identifiable

--- Web/Models/Entities/TicketComment.php
+++ Web/Models/Entities/TicketComment.php
@@ -28,6 +28,35 @@ class TicketComment extends RowModel
         return (new Users)->get($this->getRecord()->user_id);
     } 
     
+    function getAgentNumber(): ?string
+    {
+        if($this->getUType() === 0)
+            return NULL;
+        
+        $salt = "kiraMiki";
+        $hash = $this->getUser()->getId() . CHANDLER_ROOT_CONF["security"]["secret"] . $salt;
+        $hash = hexdec(substr(hash("adler32", $hash), 0, 3));
+        $hash = ceil(($hash * 999) / 4096); # proportionalize to 0-999
+        
+        return str_pad((string) $hash, 3, "0", STR_PAD_LEFT);
+    }
+    
+    function getColorRotation(): ?int
+    {
+        if(is_null($agent = $this->getAgentNumber()))
+            return NULL;
+        
+        $agent    = (int) $agent;
+        $rotation = $agent > 500 ? ( ($agent * 360) / 999 ) : $agent; # cap at 360deg
+        $values   = [0, 45, 160, 220, 310, 345]; # good looking colors
+        usort($values, function($x, $y) use ($rotation) {
+            # find closest
+            return  abs($x - $rotation) - abs($y - $rotation);
+        });
+        
+        return array_shift($values);
+    }
+    
     function getContext(): string
     {
         return $this->getRecord()->text;
