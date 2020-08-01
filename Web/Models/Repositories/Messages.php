9dc25aa938fe0f654c9720b6367350bb0330b932
Alma Armas <celestine@vriska.ru>
2020-08-01 14:23:43 +0000

    Refactor Messages::getCorrespondence

--- Web/Models/Repositories/Messages.php
+++ Web/Models/Repositories/Messages.php
@@ -17,12 +17,21 @@ class Messages
         $this->messages = $this->context->table("messages");
     }
     
-    function getCorrespondencies(RowModel $correspondent, int $page = 1, ?int $perPage = NULL): \Traversable
+    function get(int $id): ?Message
+    {
+        $msg = $this->messages->get($id);
+        if(!$msg)
+            return NULL;
+        
+        return new Message($msg);
+    }
+    
+    function getCorrespondencies(RowModel $correspondent, int $page = 1, ?int $perPage = NULL, ?int $offset = NULL): \Traversable
     {
         $id      = $correspondent->getId();
         $class   = get_class($correspondent);
         $limit   = $perPage ?? OPENVK_DEFAULT_PER_PAGE;
-        $offset  = ($page - 1) * $limit;
+        $offset  = $offset ?? ($page - 1) * $limit;
         $query   = file_get_contents(__DIR__ . "/../sql/get-correspondencies.tsql");
         DatabaseConnection::i()->getConnection()->query(file_get_contents(__DIR__ . "/../sql/mysql-msg-fix.tsql"));
         $coresps = DatabaseConnection::i()->getConnection()->query($query, $id, $class, $id, $class, $limit, $offset);
