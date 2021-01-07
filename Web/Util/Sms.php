92c9320eae4f2cf7544031b9c35e74f9e9e365b8
Alma Armas <celestine@vriska.ru>
2021-01-07 16:25:09 +0000

    Change SMS provider
    Co-authored-by: Celestine <34442450+rem-pai@users.noreply.github.com>

--- Web/Util/Sms.php
+++ Web/Util/Sms.php
@@ -1,18 +1,31 @@
 <?php declare(strict_types=1);
 namespace openvk\Web\Util;
-use Zadarma_API\{Api as ZApi, ApiException as ZException};
 
 class Sms
 {
-    function send(string $to, string $message): bool
+    static function send(string $to, string $message): bool
     {
-        $conf = (object) OPENVK_ROOT_CONF["openvk"]["credentials"]["zadarma"];
-        if(!$conf->enable) return false;
+        $conf = (object) OPENVK_ROOT_CONF["openvk"]["credentials"]["smsc"];
+        if(!$conf->enable)
+            return false;
         
-        try {
-            $api = new ZApi($conf->key, $conf->secret, false);
-            $res = $api->sendSms($to, $message, $conf->callerId);
-        } catch(ZException $e) {
+        $args = http_build_query([
+            "login"    => $conf->client,
+            "psw"      => $conf->secret,
+            "phones"   => $to,
+            "mes"      => $message,
+            "flash"    => 1,
+            "translit" => 2,
+            "fmt"      => 2,
+        ]);
+        
+        $response = file_get_contents("https://smsc.ru/sys/send.php?$args");
+        if(!$response)
+            return false;
+        
+        $response = new \SimpleXMLElement($response);
+        if(isset($response->error_code)) {
+            trigger_error("Could not send SMS to $to: $response->error (Exception $response->error_code)", E_USER_WARNING);
             return false;
         }
         
