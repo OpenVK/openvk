734a86b799b1c8144dc52c7a4c2030285843c4ef
veselcraft <veselcraft@icloud.com>
2021-10-23 00:00:25 +0300

    VKAPI: Added Audio method (early implementation :troll:)

---
+++ VKAPI/Handlers/Audio.php
@@ -0,0 +1,22 @@
+<?php declare(strict_types=1);
+namespace openvk\VKAPI\Handlers;
+
+final class Audio extends VKAPIRequestHandler
+{
+	function get(): object
+	{
+		$serverUrl = ovk_scheme(true) . $_SERVER["SERVER_NAME"];
+	
+		return (object) [
+			"count" => 1,
+			"items" => [(object) [
+				"id" => 1,
+				"owner_id" => 1,
+				"artist" => "В ОВК ПОКА НЕТ МУЗЫКИ",
+				"title" => "ЖДИТЕ :)))",
+				"duration" => 22,
+				"url" => $serverUrl . "/assets/packages/static/openvk/audio/nomusic.mp3"
+			]]
+		];
+	}
+}
