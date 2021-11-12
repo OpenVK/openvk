5556c88e4402b3ec93f48307d750973928022578
Maxim Leshchenko <50026114+maksalees@users.noreply.github.com>
2021-11-12 15:31:23 +0200

    Improvements related to group admins (#278)
    Changes:
    1. Add the ability to display only administrators on the page with a list of subscribers
    2. Add the ability to hide the fact that the subscriber is an administrator
    3. Fix display of large text in the block with the list of administrators
    4. Fix display of the number of administrators

--- Web/Models/Entities/Club.php
+++ Web/Models/Entities/Club.php
@@ -94,6 +94,11 @@ class Club extends RowModel
     {
         return is_null($this->getRecord()->owner_comment) ? "" : $this->getRecord()->owner_comment;
     }
+
+    function isOwnerHidden(): bool
+    {
+        return (bool) $this->getRecord()->owner_hidden;
+    }
     
     function isOwnerClubPinned(): bool
     {
@@ -274,9 +279,11 @@ class Club extends RowModel
         }
     }
     
-    function getManagers(int $page = 1): \Traversable
+    function getManagers(int $page = 1, bool $ignoreHidden = false): \Traversable
     {
         $rels = $this->getRecord()->related("group_coadmins.club")->page($page, 6);
+        if($ignoreHidden)
+            $rels = $rels->where("hidden", false);
         
         foreach($rels as $rel) {
             $rel = (new Managers)->get($rel->id);
@@ -286,13 +293,21 @@ class Club extends RowModel
         }
     }
 
-    function getManager(User $user): ?Manager
+    function getManager(User $user, bool $ignoreHidden = false): ?Manager
     {
-        return (new Managers)->getByUserAndClub($user->getId(), $this->getId());
+        $manager = (new Managers)->getByUserAndClub($user->getId(), $this->getId());
+
+        if ($ignoreHidden && $manager !== null && $manager->isHidden())
+            return null;
+
+        return $manager;
     }
     
-    function getManagersCount(): int
+    function getManagersCount(bool $ignoreHidden = false): int
     {
+        if($ignoreHidden)
+            return sizeof($this->getRecord()->related("group_coadmins.club")->where("hidden", false)) + (int) !$this->isOwnerHidden();
+
         return sizeof($this->getRecord()->related("group_coadmins.club")) + 1;
     }
     
