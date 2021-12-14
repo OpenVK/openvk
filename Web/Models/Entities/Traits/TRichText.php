7dc3e8965d33ec4e1986b618f22e302fe9fcebe4
veselcraft <veselcraft@icloud.com>
2021-12-14 15:28:35 +0300

    Wall: Ability to ping users by short adresses
    Fixes #28

--- Web/Models/Entities/Traits/TRichText.php
+++ Web/Models/Entities/Traits/TRichText.php
@@ -59,9 +59,9 @@ trait TRichText
             if($proc) {
                 $rel  = $this->isAd() ? "sponsored" : "ugc";
                 $text = $this->formatLinks($text);
-                $text = preg_replace("%@(id|club)([0-9]++) \(([\p{L} 0-9]+)\)%Xu", "[$1$2|$3]", $text);
-                $text = preg_replace("%@(id|club)([0-9]++)%Xu", "[$1$2|@$1$2]", $text);
-                $text = preg_replace("%\[(id|club)([0-9]++)\|([\p{L} 0-9@]+)\]%Xu", "<a href='/$1$2'>$3</a>", $text);
+                $text = preg_replace("%@([A-Za-z0-9]++) \(([\p{L} 0-9]+)\)%Xu", "[$1|$2]", $text);
+                $text = preg_replace("%@([A-Za-z0-9]++)%Xu", "[$1|@$1]", $text);
+                $text = preg_replace("%\[([A-Za-z0-9]++)\|([\p{L} 0-9@]+)\]%Xu", "<a href='/$1'>$2</a>", $text);
                 $text = preg_replace("%(#([\p{L}_-]++[0-9]*[\p{L}_-]*))%Xu", "<a href='/feed/hashtag/$2'>$1</a>", $text);
                 $text = $this->formatEmojis($text);
             }
