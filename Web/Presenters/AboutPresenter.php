3d44361a76ba29671581df1c8b26b7819ad1c4dd
Maxim Leshchenko <cnmaks90@gmail.com>
2021-12-22 18:21:38 +0200

    L10n: Improve the work of exporting a language to JS with includes

--- Web/Presenters/AboutPresenter.php
+++ Web/Presenters/AboutPresenter.php
@@ -75,7 +75,7 @@ final class AboutPresenter extends OpenVKPresenter
         if(is_null($lg))
             $this->throwError(404, "Not found", "Language is not found");
         header("Content-Type: application/javascript");
-        echo "window.lang = " . json_encode($localizer->export($lang)) . ";"; // привет хардкод :DDD
+        echo "window.lang = " . json_encode(array_unique($localizer->export($lang))) . ";"; // привет хардкод :DDD
         exit;
     }
 
