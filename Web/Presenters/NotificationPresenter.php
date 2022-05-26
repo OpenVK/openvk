39c85451fca7499ae91a69a420b04a43b2ef3adc
veselcraft <veselcraft@icloud.com>
2022-05-26 15:26:38 +0300

    Notifications: Fix code style + little bit of performance

--- Web/Presenters/NotificationPresenter.php
+++ Web/Presenters/NotificationPresenter.php
@@ -8,7 +8,7 @@ final class NotificationPresenter extends OpenVKPresenter
         $this->assertUserLoggedIn();
 
         $archive = $this->queryParam("act") === "archived";
-        $count = $this->user->identity->getNotificationsCount($archive);
+        $count   = $this->user->identity->getNotificationsCount($archive);
 
         if($count == 0 && $this->queryParam("act") == NULL) {
             $mode = "archived";
@@ -20,7 +20,7 @@ final class NotificationPresenter extends OpenVKPresenter
         $this->template->mode     = $mode;
         $this->template->page     = (int) ($this->queryParam("p") ?? 1);
         $this->template->iterator = iterator_to_array($this->user->identity->getNotifications($this->template->page, $archive));
-        $this->template->count    = $this->user->identity->getNotificationsCount($archive);
+        $this->template->count    = $count;
         
         $this->user->identity->updateNotificationOffset();
         $this->user->identity->save();
