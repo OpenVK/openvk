d4cc8d6271d3054b1e1d10645b6880192fd1716e
Ilya Prokopenko <holoniix@proton.me>
2022-06-25 00:08:46 +0700

    Revert "SQL: Fix rating and notifications"
    This reverts commit e1072738fd23b4f45597186965a2f862f1cd096b.

--- install/sqls/00026-rating-fixed.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `profiles` CHANGE `coins` `coins` bigint(20) unsigned NOT NULL DEFAULT '0' AFTER `email`;
-ALTER TABLE `profiles` ADD COLUMN `rating` bigint(20) unsigned NOT NULL DEFAULT '0' AFTER `coins`;
