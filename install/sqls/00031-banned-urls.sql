5f3928e7ace39d9bb3323953a7fb27d9971bf50e
n1rwana <93197434+n1rwana@users.noreply.github.com>
2022-08-27 02:17:16 +0300

    SQL

--- install/sqls/00031-banned-urls.sql
+++ install/sqls/00031-banned-urls.sql
@@ -1,13 +1,14 @@
 CREATE TABLE `links_banned` (
-  `id` bigint NOT NULL,
-  `link` text NOT NULL,
-  `reason` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
-  `initiator` bigint NOT NULL
+    `id` bigint NOT NULL,
+    `domain` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+    `regexp_rule` text NOT NULL,
+    `reason` text CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci,
+    `initiator` bigint NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 ALTER TABLE `links_banned`
-  ADD PRIMARY KEY (`id`);
+    ADD PRIMARY KEY (`id`);
 
 ALTER TABLE `links_banned`
-  MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
+    MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
 COMMIT;
