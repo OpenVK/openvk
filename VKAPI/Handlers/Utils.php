3e330e5959855bf12c7134d8851bc25823a766f4
NoPlagiarism <37241775+NoPlagiarism@users.noreply.github.com>
2023-06-03 14:00:51 +0500

    Finally re-add Utils.resolveScreenName

--- VKAPI/Handlers/Utils.php
+++ VKAPI/Handlers/Utils.php
@@ -1,10 +1,46 @@
 <?php declare(strict_types=1);
 namespace openvk\VKAPI\Handlers;
+use openvk\Web\Models\Repositories\{Users, Clubs};
 
 final class Utils extends VKAPIRequestHandler
 {
     function getServerTime(): int
     {
         return time();
+    }
+
+    function resolveScreenName(string $screen_name): object
+    {
+        if(\Chandler\MVC\Routing\Router::i()->getMatchingRoute("/$screen_name")[0]->presenter !== "UnknownTextRouteStrategy") {
+            if(substr($screen_name, 0, strlen("id")) === "id") {
+                return (object) [
+                    "object_id" => (int) substr($screen_name, strlen("id")),
+                    "type"      => "user"
+                ];
+            } else if(substr($screen_name, 0, strlen("club")) === "club") {
+                return (object) [
+                    "object_id" => (int) substr($screen_name, strlen("club")),
+                    "type"      => "group"
+                ];
+            }
+        } else {
+            $user = (new Users)->getByShortURL($screen_name);
+            if($user) {
+                return (object) [
+                    "object_id" => $user->getId(),
+                    "type"      => "user"
+                ];
+            }
+
+            $club = (new Clubs)->getByShortURL($screen_name);
+            if($club) {
+                return (object) [
+                    "object_id" => $club->getId(),
+                    "type"      => "group"
+                ];
+            }
+
+            return (object) [];
+        }
     }
 }
