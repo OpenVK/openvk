f909976348ec6cbf41b61d6c6941f9282a01bdfb
veselcraft <veselcraft@icloud.com>
2023-08-04 00:09:31 +0300

    Wall: design + sql fix

--- install/sqls/00038-posts-geo.sql
+++ install/sqls/00038-posts-geo.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `Posts`
+ALTER TABLE `posts`
     ADD `geo`     LONGTEXT       NULL DEFAULT NULL AFTER `deleted`,
     ADD `geo_lat` DECIMAL(10, 8) NULL DEFAULT NULL AFTER `geo`,
     ADD `geo_lon` DECIMAL(10, 8) NULL DEFAULT NULL AFTER `geo_lat`;
