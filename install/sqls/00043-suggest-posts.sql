2cc1b03cd1748516b70d55d3ba63ddbfb010c75b
lalka2018 <99399973+lalka2016@users.noreply.github.com>
2023-11-16 16:07:13 +0300

    Add indesk

--- install/sqls/00043-suggest-posts.sql
+++ install/sqls/00043-suggest-posts.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `posts` ADD `suggested` TINYINT(2) UNSIGNED NOT NULL DEFAULT '0' AFTER `deleted`; 
+ALTER TABLE `posts` ADD `suggested` TINYINT(2) UNSIGNED NOT NULL DEFAULT '0' AFTER `deleted`, ADD INDEX (`suggested`); 
