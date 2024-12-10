7197863f00283a5f3d6eec2e8892554042d7052e
mrilyew <99399973+mrilyew@users.noreply.github.com>
2024-12-10 17:24:32 +0300

    make compatible with vk api

--- VKAPI/Handlers/Reports.php
+++ VKAPI/Handlers/Reports.php
@@ -5,37 +5,49 @@ use openvk\Web\Models\Repositories\Reports as ReportsRepo;
 
 final class Reports extends VKAPIRequestHandler
 {
-    function add(string $type, int $id, string $reason): object
+    function add(int $owner_id = 0, string $comment = "", int $reason = 0, string $type = "", string $report_source = ""): int
     {
         $this->requireUser();
         $this->willExecuteWriteAction();
 
-        if ($id <= 0) {
-            $this->fail(100, "ID must be a positive number");
+        $allowed_types = ["post", "photo", "video", "group", "comment", "note", "app", "user", "audio"];
+        if($type == "" || !in_array($type, $allowed_types)) {
+            $this->fail(100, "One of the parameters specified was missing or invalid: type should be ".implode(", ", $allowed_types));
         }
 
-        if(mb_strlen(trim($reason)) === 0) {
-            $this->fail(100, "Reason can't be empty");
+        if($owner_id <= 0) {
+            $this->fail(100, "One of the parameters specified was missing or invalid: Bad input");
         }
 
-        if ($type === "user" && $id === $this->getUser()->getId()) {
-            $this->fail(100, "You can't report yourself");
+        if(mb_strlen($comment) === 0) {
+            $this->fail(100, "One of the parameters specified was missing or invalid: Comment can't be empty");
         }
 
-        if(in_array($type, ["post", "photo", "video", "group", "comment", "note", "app", "user"])) {
-            if (count(iterator_to_array((new ReportsRepo)->getDuplicates($type, $id, NULL, $this->getUser()->getId()))) <= 0) {
-                $report = new Report;
-                $report->setUser_id($this->getUser()->getId());
-                $report->setTarget_id($id);
-                $report->setType($type);
-                $report->setReason($reason);
-                $report->setCreated(time());
-                $report->save();
-            }
-
-            return (object) [ "reason" => $reason ];
-        } else {
-            $this->fail(3, "Unable to submit a report on this content type");
+        if($type == "user" && $owner_id == $this->getUser()->getId()) {
+            return 1;
         }
+
+        if($this->getUser()->isBannedInSupport()) {
+            return 0;
+        }
+
+        if(sizeof(iterator_to_array((new ReportsRepo)->getDuplicates($type, $owner_id, NULL, $this->getUser()->getId()))) > 0) {
+            return 1;
+        }
+
+        try {
+            $report = new Report;
+            $report->setUser_id($this->getUser()->getId());
+            $report->setTarget_id($owner_id);
+            $report->setType($type);
+            $report->setReason($comment);
+            $report->setCreated(time());
+
+            $report->save();
+        } catch(\Throwable $e) {
+            $this->fail(-1, "Unknown error failed");
+        }
+
+        return 1;
     }
 }
--- Web/Presenters/ReportPresenter.php
+++ Web/Presenters/ReportPresenter.php
@@ -91,7 +91,7 @@ final class ReportPresenter extends OpenVKPresenter
             exit(json_encode([ "error" => tr("error_segmentation") ]));
       
         if ($this->queryParam("type") === "user" && $id === $this->user->id)
-            exit(json_encode(["reason" => "You can't report yourself"]));
+            exit(json_encode([ "error" => "You can't report yourself" ]));
 
         if(in_array($this->queryParam("type"), ["post", "photo", "video", "group", "comment", "note", "app", "user", "audio"])) {
             if (count(iterator_to_array($this->reports->getDuplicates($this->queryParam("type"), $id, NULL, $this->user->id))) <= 0) {
