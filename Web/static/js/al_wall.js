04d24aaba3732a98ffba847bd83ff87953e1bda2
koke228666 <146654074+koke228666@users.noreply.github.com>
2025-03-07 22:51:31 +0300

    make creating avatar ✨better✨

--- Web/static/js/al_wall.js
+++ Web/static/js/al_wall.js
@@ -2179,9 +2179,11 @@ $(document).on("click", "#add_image", (e) => {
                         
                         document.querySelector("#bigAvatar").src = response.url
                         document.querySelector("#bigAvatar").parentNode.href = "/photo" + response.new_photo
-                    
-                        document.querySelector(".add_image_text").style.display = "none"
+						
                         document.querySelector(".avatar_controls").style.display = "block"
+                        document.querySelector(".avatar_controls .set_image").style.display = "block"
+						document.querySelector(".avatar_controls .avatarDelete").style.display = "block"
+                        document.querySelector(".avatar_controls .upload_image").style.display = "none"
                     }
                 })
             })
@@ -2314,8 +2316,9 @@ $(document).on("click", ".avatarDelete", (e) => {
                     document.querySelector("#bigAvatar").parentNode.href = response.new_photo ? ("/photo" + response.new_photo) : "javascript:void(0)"
                     
                     if(!response.has_new_photo) {
-                        document.querySelector(".avatar_controls").style.display = "none"
-                        document.querySelector(".add_image_text").style.display = "block"
+                        document.querySelector(".avatar_controls .set_image").style.display = "none"
+						document.querySelector(".avatar_controls .avatarDelete").style.display = "none"
+                        document.querySelector(".avatar_controls .upload_image").style.display = "block"
                     }
                 }
             })
