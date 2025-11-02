e78cbb105952c046762e8e641307b691158c00e7
Diablo <80642969+progzone122@users.noreply.github.com>
2025-11-02 15:07:06 +0200

    fix(ui): prevent hidden .toTop from overlapping buttons (#1429)
    ## Проблема
    Элемент с классом `.toTop` изначально задаётся как `block` с
    фиксированной шириной и высотой. При увеличении масштаба страницы этот
    невидимый и бесполезный элемент **перекрывает левое меню с кнопками**,
    даже когда скроллинга страницы еще не было.
    
    
    https://github.com/user-attachments/assets/7b23a129-d59c-4518-8cfc-e753018dda59
    
    ## Предлагаемое решение
    Задаём для элемента с классом `.toTop` стиль `display: none;` **по
    умолчанию** и управляем его состоянием через JS.
    Для удобства создан отдельный класс `.hidden`, чтобы проще менять это
    значение.
    
    
    https://github.com/user-attachments/assets/36121559-c2b2-4df4-bbc8-c53bbe2fc0f9

--- Web/static/js/scroll.js
+++ Web/static/js/scroll.js
@@ -1,35 +1,39 @@
-window.addEventListener("scroll", function(e) {
-    if(window.scrollY < 100) {
-        if(window.temp_y_scroll) {
-            u('.toTop').addClass('has_down')
-        }
-        document.body.classList.toggle("scrolled", false);
-    } else {
-        document.body.classList.toggle("scrolled", true);
-        u('.toTop').removeClass('has_down')
+window.addEventListener("scroll", function (e) {
+  if (window.scrollY < 100) {
+    if (window.temp_y_scroll) {
+      u(".toTop").addClass("has_down");
+      u(".toTop").removeClass("hidden");
     }
+
+    document.body.classList.toggle("scrolled", false);
+    u(".toTop").addClass("hidden");
+  } else {
+    document.body.classList.toggle("scrolled", true);
+    u(".toTop").removeClass("has_down");
+    u(".toTop").removeClass("hidden");
+  }
 });
 
-u(".toTop").on("click", function(e) {
-    const y_scroll = window.scrollY
-    const scroll_margin = 20
+u(".toTop").on("click", function (e) {
+  const y_scroll = window.scrollY;
+  const scroll_margin = 20;
 
-    if(y_scroll > 100) {
-        window.temp_y_scroll = y_scroll
-        window.scrollTo(0, scroll_margin)
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        })
-    } else {
-        if(window.temp_y_scroll) {
-            window.scrollTo(0, window.temp_y_scroll - scroll_margin)
-            window.scrollTo({
-                top: window.temp_y_scroll,
-                behavior: "smooth"
-            })
-        }
+  if (y_scroll > 100) {
+    window.temp_y_scroll = y_scroll;
+    window.scrollTo(0, scroll_margin);
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  } else {
+    if (window.temp_y_scroll) {
+      window.scrollTo(0, window.temp_y_scroll - scroll_margin);
+      window.scrollTo({
+        top: window.temp_y_scroll,
+        behavior: "smooth",
+      });
     }
+  }
 
-    u(document).trigger('scroll')
-})
+  u(document).trigger("scroll");
+});
