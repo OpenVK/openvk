f3abdfc64b864d184641e868693ed6f1afdd9887
Vladimir Barinov <veselcraft@icloud.com>
2026-02-05 16:29:46 -0500

    chore: g lint is g list

--- Web/Presenters/WallPresenter.php
+++ Web/Presenters/WallPresenter.php
@@ -257,7 +257,7 @@ final class WallPresenter extends OpenVKPresenter
 
     public function renderHashtagFeed($hashtag): void
     {
-        $hashtag = rawurldecode(''.$hashtag); // simpler than converting it with countless ifs
+        $hashtag = rawurldecode('' . $hashtag); // simpler than converting it with countless ifs
 
         $page  = (int) ($_GET["p"] ?? 1);
         $posts = $this->posts->getPostsByHashtag($hashtag, $page);
