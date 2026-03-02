51a7a2c5e33b0de83d3ed1c696a909b916e5bca8
Sergey <81107844+LeenzeryDev@users.noreply.github.com>
2026-03-02 18:24:24 +0500

    fix: correctly determine author for reply mentions (#1534)
    хз вроде починил #1515

--- Web/static/js/al_comments.js
+++ Web/static/js/al_comments.js
@@ -2,11 +2,11 @@ u(document).on("click", ".comment-reply", function(e) {
     let comment   = u(e.target).closest(".post");
     let authorId  = comment.data("owner-id");
     let authorNm  = u(".post-author > a > b", comment.first()).text().trim();
-    let fromGroup = Boolean(comment.data("from-group"));
+    let fromGroup = comment.attr("data-from-group") === "true";
     let postId    = comment.data("post-id");
     let inputbox  = postId == null ? u("#write textarea") : u("#wall-post-input" + (postId || ""));
     let mention   = ("[" + (fromGroup ? "club" : "id") + authorId + "|" + authorNm + "], ");
-    
+
     // Substitute pervious mention if present, prepend otherwise
     inputbox.nodes.forEach(node => {
         node.value = node.value.replace(/(^\[([A-Za-z0-9]+)\|([\p{L} 0-9@]+)\], |^)/u, mention);
