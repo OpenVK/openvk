e5942fcef63e5c897844a44d48c35fa0232b5aed
v00d00mag1c <234468996+v00d00mag1c@users.noreply.github.com>
2026-04-30 15:43:13 +0300

    add ability to change chat

--- Web/static/js/messages/conversations_list.js
+++ Web/static/js/messages/conversations_list.js
@@ -1,62 +1,67 @@
 class ConversationsViewModel {
-    constructor(initial = []) {
-        this.items = initial.items
-        this.conversations = ko.observableArray(this.convs);
+    constructor() {
+        this.conversations = ko.observableArray(window.im.conversations.convs);
     }
 
-    selectChat(conv) {
-        window.messenger.
-        this.openMessenger(conv)
-    }
 }
 
-window.conversations = new (class {
-  constructor() {
-    this.template = `
-    <div class="crp-list scroll_container">
-        <div data-bind="foreach: window.conversations.view.items">
-            <div class="scroll_node crp-entry" data-bind="event: { click: function(data, event) { window.conversations.view.selectChat(this) } }">
-                <div class="crp-entry--image">
-                    <img data-bind="attr: { src: peer.avatar_any }"
-                    loading="lazy" />
+class Conversations {
+    constructor() {
+        this.template = `
+        <div class="crp-list scroll_container">
+            <div data-bind="foreach: window.im.conversations.convs">
+                <div class="scroll_node crp-entry" data-bind="event: { click: function(data, event) { window.im.selectChat(this) } }">
+                    <div class="crp-entry--image">
+                        <img data-bind="attr: { src: peer.avatar_any }"
+                        loading="lazy" />
+                    </div>
+                    <div class="crp-entry--info">
+                        <a data-bind="attr: { href: peer.chat_url }, html: peer.name "></a><br/>
+                    </div>
+                    <div class="crp-entry--message"></div>
                 </div>
-                <div class="crp-entry--info">
-                    <a data-bind="attr: { href: peer.chat_url }, html: peer.name "></a><br/>
-                </div>
-                <div class="crp-entry--message"></div>
             </div>
         </div>
-    </div>
-    `
-  }
-
-  async getConversations() {
-    // adding profiles to conversation items
-    let convs = await window.OVKAPI.call("messages.getConversations", {
-        extended: 1,
-        fields: 'photo_100'
-    });
-    convs.items.forEach(item => {
-        const _id = item.conversation.peer.id
-        const author = find_author(_id, convs.profiles, convs.groups)
-
-        item.peer = new ChatGeneralForm(author);
-        console.log(item)
-    })
-
-    return convs;
-  }
-
-  async render(container = null) {
-    container.insertAdjacentHTML('beforeend', this.template)
-    // todo change
-    if (window.OVKAPI == null) {
-        await new Promise((resolve) => setTimeout(resolve, 1000));
+        `
     }
 
-    const convs = await this.getConversations();
-    this.view = new ConversationsViewModel(convs);
-    console.log(conversations.view.convs)
-    ko.applyBindings(this.view, container);
-  }
-})();
+    async getConversations() {
+        // adding profiles to conversation items
+        let convs = await window.OVKAPI.call("messages.getConversations", {
+            extended: 1,
+            fields: 'photo_100'
+        });
+        convs.items.forEach(item => {
+            const _id = item.conversation.peer.id
+            const author = find_author(_id, convs.profiles, convs.groups)
+
+            item.peer = new ChatGeneralForm(author);
+            console.log(item)
+        })
+
+        return convs.items;
+    }
+
+    async init() {
+        this.convs = await this.getConversations();
+        this.view = new ConversationsViewModel();
+    }
+
+    appear(container) {
+        if (this.appeared) {
+            container.classList.remove('hidden');
+
+            return;
+        }
+
+        this.node = container.insertAdjacentHTML('beforeend', this.template);
+        this.appeared = true;
+
+        // todo unapply when unused???
+        ko.applyBindings(this.view, container);
+    }
+
+    hide(container) {
+        container.classList.add('hidden')
+    }
+}
--- Web/static/js/messages/messenger.js
+++ Web/static/js/messages/messenger.js
@@ -1,40 +1,137 @@
-function MessengerViewModel(initialMessages = []) {
-    window.messages     = ko.observableArray(initialMessages);
-    this.messages       = window.messages;
-    this.messageContent = ko.observable("");
-    
-    this.sendMessage = model => {
-        if(model.messageContent() === "") return false;
-        
-        window.Msg.sendMessage(model.messageContent());
-        model.messageContent("");
-    };
-    this.loadHistory = _ => {
-        window.Msg._loadHistory();
-    };
-    
-    this.onMessagesScroll   = (model, e) => {
-        if(e.target.scrollTop < 21)
-            model.loadHistory();
-    };
-    this.onTextareaKeyPress = (model, e) => {
-        if(e.which === 13) {
-            if(!e.metaKey && !e.shiftKey) {
-                let ta = u("textarea[name=message]").nodes[0];
-                ta.blur(); //Fix update
-                model.sendMessage(model);
-                ta.focus();
-                
-                return false;
-            }
+class Messenger {
+    constructor() {
+        this.opened_tabs = [];
+        this.template = `
+    <div class="messenger-app">
+        <div class="messenger-app--messages">
+            <div data-bind="foreach: messages">
+                <div class="messenger-app--messages---message" data-bind="css: { unread: !read }">
+                    <img class="ava" data-bind="attr: { src: sender.avatar, alt: sender.name }" />
+                    <div class="_content">
+                        <a href="#" data-bind="attr: { href: sender.link }">
+                            <strong data-bind="text: sender.name"></strong>
+                        </a>
+                        <span class="text" data-bind="html: text"></span>
+                        <div data-bind="foreach: attachments" class="attachments">
+                            <div class="msg-attach-j">
+                                <div data-bind="if: type === 'photo'" class="msg-attach-j-photo">
+                                    <a data-bind="attr: { href: link }">
+                                        <img data-bind="attr: { src: photo.url, alt: photo.caption  }" />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="time" align="right">
+                        <span data-bind="html: timing.sent"></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="messenger-app--input">
+            <img class="ava" alt="{$thisUser->getCanonicalName()}" />
+            <div class="messenger-app--input---messagebox">
+                <textarea
+                        data-bind="value: messageContent, event: { keydown: onTextareaKeyPress }"
+                        name="message"
+                        placeholder="{_enter_message}"></textarea>
+                <button class="button" data-bind="click: sendMessage">{_send}</button>
+            </div>
+            <img class="ava" alt="{$correspondent->getCanonicalName()}" />
+        </div>
+    </div>
+        `
+    }
+
+    appear(container = null) {
+        if (this.appeared) {
+            container.classList.remove('hidden');
+            return;
         }
-        
-        return true;
-    };
+
+        this.appeared = true;
+        container.insertAdjacentHTML('beforeend', this.template);
+
+        ko.applyBindings(this.view, container);
+    }
+
+    hide(container) {
+        container.classList.add('hidden');
+    }
+
+    selectChat() {
+
+    }
 }
 
-window.messenger = new (class {
-    constructor() {}
+window.im = new (class {
+    constructor() {
+        this.tabs = ['conversations', 'messenger'];
+        this.tab = ''
+    }
 
-    
+    async init(container) {
+        // todo change
+        if (window.OVKAPI == null) {
+            await new Promise((resolve) => setTimeout(resolve, 1000));
+        }
+
+        this.root = container;
+        this._initTabs();
+
+        this.conversations = new Conversations();
+        await this.conversations.init();
+        this.messenger = new Messenger();
+        this.selectTab('conversations');
+    }
+
+    selectTab(tab_name) {
+        if (this.tabs.indexOf(tab_name) == -1) {
+            throw new Error('invalid tab');
+        }
+
+        this.tab = tab_name;
+        // its so amazing to define check state via id not by class
+        // and do this for tab's text
+        u(this.root).find(".tabs .tab").attr('id', '')
+        u(this.root).find(`.tabs .tab[data-tab='${tab_name}']`).attr('id', 'activetabs')
+
+        switch (tab_name) {
+            case 'conversations':
+                this.conversations.appear(this._getTabWindow('conversations'));
+                this.messenger.hide(this._getTabWindow('messenger'));
+                break;
+            case 'messenger':
+                this.conversations.hide(this._getTabWindow('conversations'));
+                this.messenger.appear(this._getTabWindow('messenger'));
+                break;
+        }
+    }
+
+    _initTabs() {
+        // todo rewrite on kojs
+        this.root.insertAdjacentHTML('beforeend', `
+            <div class="tabs"></div>
+        `);
+
+        this.tabs.forEach(tab => {
+            this.root.insertAdjacentHTML('beforeend', `
+                <div data-window="${tab}"></div>    
+            `);
+            this.root.querySelector(".tabs").insertAdjacentHTML('beforeend', `
+                <a data-tab="${tab}" onclick="window.im.selectTab('${tab}', event)" class="tab">
+                    ${tab}
+                </a>
+            `);
+        })
+    }
+
+    _getTabWindow(tab_name) {
+        return this.root.querySelector(`div[data-window="${tab_name}"]`)
+    }
+
+    selectChat(conv) {
+        this.selectTab('messenger');
+        this.messenger.selectChat(conv);
+    }
 })()
