bfd9ddf7a0366390cb77bd6071158246fb01dba6
NoPlagiarism <37241775+NoPlagiarism@users.noreply.github.com>
2026-06-09 11:15:32 +0500

    feat(links, away): redirect any mirror domain to current one

--- Web/Presenters/AwayPresenter.php
+++ Web/Presenters/AwayPresenter.php
@@ -11,16 +11,27 @@ final class AwayPresenter extends OpenVKPresenter
 {
     public function renderAway(): void
     {
-        $checkBanEntries = (new BannedLinks())->check($this->queryParam("to"));
+        $redirTo = $this->queryParam("to");
         if (OPENVK_ROOT_CONF["openvk"]["preferences"]["susLinks"]["warnings"]) {
+            $checkBanEntries = (new BannedLinks())->check($redirTo);
             if (sizeof($checkBanEntries) > 0) {
                 $this->pass("openvk!Away->view", $checkBanEntries[0]);
             }
         }
 
+        if (isset(OPENVK_ROOT_CONF["openvk"]["mirrors"])) {
+            $uri = str_replace(["https://", "http://"], "", $redirTo);
+            $domainTo = explode("/", $uri)[0];
+            $isMirror = in_array(str_replace("www.", "", $domainTo), OPENVK_ROOT_CONF["openvk"]["mirrors"]);
+            if ($isMirror) {
+                $currentDomain = $_SERVER["SERVER_NAME"];
+                $redirTo = str_replace($domainTo, $currentDomain, $redirTo);
+            }
+        }
+
         header("HTTP/1.0 302 Found");
         header("X-Robots-Tag: noindex, nofollow, noarchive");
-        header("Location: " . rawurldecode($this->queryParam("to")));
+        header("Location: " . rawurldecode($redirTo));
         exit;
     }
 
