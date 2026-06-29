290163451016025606a9d04b94478c4d78650ef6
crythoughts <234468996+crythoughts@users.noreply.github.com>
2026-06-29 14:31:29 +0300

    1

--- Web/Models/Entities/Playlist.php
+++ Web/Models/Entities/Playlist.php
@@ -304,7 +304,7 @@ class Playlist extends MediaCollection
         $props[] = tr("audios_count", $this->size());
         $props[] = "<span id='listensCount'>" . tr("listens_count", $this->getListens()) . "</span>";
         if ($length > 0) {
-            $props[] = "<span>" . tr("minutes_count", $length)."</span>";
+            $props[] = "<span>" . tr("minutes_count", $length) . "</span>";
         }
         $props[] = "<span>" . tr("created_playlist") . " " . $this->getPublicationTime() . "</span>";
         # if($this->getEditTime()) $props[] = tr("updated_playlist") . " " . $this->getEditTime();
