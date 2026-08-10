1b01acd8800295da66c804a434df85eccec52ff2
Vladimir Barinov <veselcraft@icloud.com>
2026-08-10 22:24:44 +0300

    chore(lint)

--- Web/Models/Entities/Comment.php
+++ Web/Models/Entities/Comment.php
@@ -107,7 +107,7 @@ class Comment extends Post
                 }
             } elseif ($attachment instanceof \openvk\Web\Models\Entities\Video) {
                 # if (VKAPI_DECL_VER_MAJOR <= 4) {
-                    $res->attachments[] = $attachment->toVkApiStruct($user);
+                $res->attachments[] = $attachment->toVkApiStruct($user);
                 /* } else {
                     $res->attachments[] = [
                         'type' => 'video',
