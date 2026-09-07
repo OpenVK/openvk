a5c94004d5ff933ea436e8ed1d2e110737700a29
Marat Sibagatullin <contact@mrksbgg.ru>
2026-09-07 14:13:00 +0300

    fix(photos): handle missing album on photo deletion

--- Web/Models/Entities/Photo.php
+++ Web/Models/Entities/Photo.php
@@ -466,8 +466,12 @@ class Photo extends Media
 
     public function delete(bool $softly = true): void
     {
-        $this->getAlbum()->removePhoto($this);
+        $album = $this->getAlbum();
 
+        if ($album !== null) {
+            $album->removePhoto($this);
+        }
+        
         parent::delete($softly);
     }
 }
