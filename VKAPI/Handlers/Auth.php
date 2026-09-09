810e47e44905c3d138ddfe425ba921bd4d2ea7e5
Vladimir Barinov <veselcraft@icloud.com>
2026-09-09 18:26:16 +0300

    feat(api): implement dummy vk id method

---
+++ VKAPI/Handlers/Auth.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace openvk\VKAPI\Handlers;
+
+use openvk\Web\Models\Repositories\{Users as UsersRepo, Clubs as ClubsRepo, Posts as PostsRepo};
+
+final class Auth extends VKAPIRequestHandler
+{
+    public function validateAccount(): object
+    {
+        // dummy function, always return passwd
+        return (object) [
+            "flow_name" => "need_password", 
+            "sid" => "1"
+        ];
+    }
+}
