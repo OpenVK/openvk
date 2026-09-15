f0f7fb147af98ac95d82320995b9de60a73065d7
Vladimir Barinov <veselcraft@icloud.com>
2026-09-15 20:32:49 +0300

    fix(clubs): some admins won't show up if they pinned the club to the left menu

--- Web/Models/Entities/Club.php
+++ Web/Models/Entities/Club.php
@@ -399,7 +399,7 @@ class Club extends RowModel
     {
         $rels = $this->getRecord()->related("group_coadmins.club")->page($page, 6);
         if ($ignoreHidden) {
-            $rels = $rels->where("club_pinned", false);
+            $rels = $rels->where("hidden", false);
         }
 
         foreach ($rels as $rel) {
